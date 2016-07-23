627213354903fa20b305df31b3345227c24f8e1e
CielNi <562421167@qq.com>
2016-07-23 21:28:30 +0800

    update _api.js

--- src/statistics/_store/_api.js
+++ src/statistics/_store/_api.js
@@ -4,5 +4,13 @@ export default {
   nano_active: {
     url: baseUrl + 'nano_active',
     serialize: 'nano_active'
+  },
+  location_active: {
+    url: baseUrl + 'location_active',
+    serialize: 'location_active'
+  },
+  location_active_detail: {
+    url: baseUrl + 'location/active/detail',
+    serialize: 'location_active_detail'
   }
 }
