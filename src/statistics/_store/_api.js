c208bc8d3ea7d9f8148946b819f6dc7d82ffeedc
CielNi <562421167@qq.com>
2016-07-25 09:58:26 +0800

    resolve conflict

--- src/statistics/_store/_api.js
+++ src/statistics/_store/_api.js
@@ -5,18 +5,8 @@ export default {
     url: baseUrl + 'nano_active',
     serialize: 'nano_active'
   },
-<<<<<<< HEAD
-  location_active: {
-    url: baseUrl + 'location_active',
-    serialize: 'location_active'
-  },
-  location_active_detail: {
-    url: baseUrl + 'location/active/detail',
-    serialize: 'location_active_detail'
-=======
   nano_active_map: {
     url: baseUrl + 'nano_active',
     serialize: 'nano_active_map'
->>>>>>> master
   }
 }
